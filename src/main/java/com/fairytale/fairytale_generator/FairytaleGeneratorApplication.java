664a1412454c3364d63063e97f96196f8572faa4
MinHyuk Kang <1rkdalsgur1@naver.com>
2024-09-10 01:47:12 +0900

    Initial commit

---
+++ src/main/java/com/fairytale/fairytale_generator/FairytaleGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.fairytale.fairytale_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairytaleGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairytaleGeneratorApplication.class, args);
+	}
+
+}
