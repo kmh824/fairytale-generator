5288406b9919ef820a3127a327ab44382fdb4f6e
Siwon <inggo113@naver.com>
2024-10-30 00:53:16 +0900

    axios 요청

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Home from './components/Home'; // 홈 화면 컴포넌트
 
 const App = () => {
@@ -8,14 +9,13 @@ const App = () => {
         const token = localStorage.getItem('token');
         if (token) {
             // 백엔드에서 토큰 검증 API 호출
-            fetch('http://localhost:8080/api/auth/validate', {
-                method: 'POST',
+            axios.post('https://fairytalegenerator.kro.kr/api/auth/validate', {}, {
                 headers: {
                     'Authorization': `Bearer ${token}`,
                 },
             })
                 .then(response => {
-                    if (response.ok) {
+                    if (response.status === 200) {
                         setIsLoggedIn(true); // 토큰이 유효하면 로그인 상태로 설정
                     } else {
                         setIsLoggedIn(false); // 토큰이 유효하지 않으면 로그인 상태 해제
@@ -31,7 +31,7 @@ const App = () => {
 
     const handleLoginClick = () => {
         // 구글 로그인 페이지로 이동
-        window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+        window.location.href = 'https://fairytalegenerator.kro.kr/oauth2/authorization/google';
     };
 
     // 구글 로그인 후 JWT 토큰을 백엔드에서 받아와 localStorage에 저장하는 부분
